038ff0ef8b4ec4c281a53840493945b4ac0f5741
William <160588135@qq.com>
2023-10-31 11:32:31 +0800

    小修改

--- PA/PA6.0.py
+++ PA/PA6.0.py
@@ -11,7 +11,7 @@ def chessboard_print(piece, delete_piece, whether_print_red):
             print(f'\033[31;1m {piece[j]} \033[0m', end='')
         else:
             print(f' {piece[j]} ', end='')
-        if j != 2 and j != 5 and j != 8:
+        if j not in (2, 5, 8):
             print('|', end='')
         else:
             print()
@@ -52,7 +52,7 @@ def win(piece):
             important_print('获胜者：玩家O', 1)
             return 2
 
-    if not' ' in piece:
+    if ' ' not in piece:
         os.system('cls')
         chessboard_print(piece, -1, 0)
         important_print('平局', 1)
@@ -61,7 +61,7 @@ def win(piece):
     return 0
 
 
-# 主函数
+# 主程序
 game_continues = ' '
 chessboard = ['r', 't', 'y', 'f', 'g', 'h', 'v', 'b', 'n']
 
@@ -88,12 +88,12 @@ while 1:
     list_piece = [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ']  # 每个位置的状态
     game_num = 1  # 循环次数的计数
     delete = -1  # 初始化要删除的内容
-    flag = '初始化flag'
+    input_error = '初始化input_error'
     while not win(list_piece):
-        if flag != '输入错误':
+        if input_error != '输入错误':
             os.system('cls')
             print(f"玩家{who}的回合：")
-        flag = '初始化flag'
+        input_error = '初始化input_error'
         if game_num > 6:  # 即将删除的内容的位置
             delete = record_num[game_num - 7]
         chessboard_print(list_piece, delete, game_mode)  # 输出棋盘
@@ -104,12 +104,12 @@ while 1:
             if where == chessboard[i]:
                 # 防止下到已经下过的地方
                 if game_num > 6 and record_num[game_num - 7] == i:
-                    flag = '落子位置即将删除'
-                if list_piece[i] != ' ' and flag != '落子位置即将删除':
+                    input_error = '落子位置即将删除'
+                if list_piece[i] != ' ' and input_error != '落子位置即将删除':
                     os.system('cls')
 
                     print('这里已经下过了，请换一个位置' + '\n' + f"玩家{who}的回合：")
-                    flag = '输入错误'
+                    input_error = '输入错误'
                     break
 
                 list_piece[i] = who
@@ -120,13 +120,13 @@ while 1:
             if i == 8:
                 os.system('cls')
                 print('棋盘上没有这个位置，请换一个位置' + '\n' + f"玩家{who}的回合：")
-                flag = '输入错误'
+                input_error = '输入错误'
                 break
-        if flag == '输入错误':
+        if input_error == '输入错误':
             continue
 
         # 保证棋盘上最多六个棋子
-        if game_num > 6 and flag != '落子位置即将删除' and game_mode:
+        if game_num > 6 and input_error != '落子位置即将删除' and game_mode:
             list_piece[record_num[game_num - 7]] = ' '
             print()
 
