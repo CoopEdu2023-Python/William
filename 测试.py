8a81fb19028cfaa91c512c4df7509649d0004707
William <160588135@qq.com>
2023-11-30 08:54:59 +0800

    homework

--- 测试.py
+++ 测试.py
@@ -1,29 +1,19 @@
-# 多态： 多种状态，即完成某个行为时，使用不同的对象会得到不同的状态。
+a = 'AEIOUaeiou'
+x = list()
+s = list(s)
+for i in s:
+    if i in a:
+        x.append(i)
 
-class Animal:  # 抽象类
-    def speak(self):
-        pass
-
-
-class Dog(Animal):
-    def speak(self):
-        print('汪汪汪')
-
-
-class Cat(Animal):
-    def speak(self):
-        print('喵喵喵')
-
-
-def make_noise(animal: Animal):
-    animal.speak()
-
-
-dog = Dog()
-cat = Cat()
-
-make_noise(dog)
-make_noise(cat)
-
-# 抽象类：含有抽象方法的类称之为抽象类
-# 抽象方法：方法体是空实现的（pass）称之为抽象方法
+num = 0
+n = 0
+r = list()
+for i in s:
+    if i in a:
+        num += 1
+        r.insert(n, x[0 - num])
+    else:
+        r.append(i)
+    n += 1
+r = ''.join(r)
+return r
