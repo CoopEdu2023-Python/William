7c8765fabf3ed937e006c71e1b8bb93b5664c32d
William <160588135@qq.com>
2023-12-06 15:43:57 +0800

    homework

--- PA2/main.py
+++ PA2/main.py
@@ -3,6 +3,14 @@ import scene
 import dinosaur
 
 
+def print_start():  # 显示开始提示
+    font = pygame.font.Font(None, 75)  # 字体设置
+    text = font.render('Press the space key to start the game', True, 'black')
+    text_rect = text.get_rect()
+    text_rect.centerx, text_rect.bottom = 1280//2, 522
+    screen.blit(text, text_rect)
+
+
 # pygame setup
 pygame.init()
 screen = pygame.display.set_mode((1280, 720))
@@ -16,8 +24,25 @@ D = dinosaur.Dinosaur()
 G = scene.Ground()
 frames = 60  # 帧率
 time = 0  # 循环次数
-# game loop
 
+pygame.display.set_caption("chrome://dino")  # 标题
+
+# game start
+while not pygame.key.get_pressed()[pygame.K_SPACE] and running:
+    screen.fill("white")
+    screen.blit(D.d_start, D.rect_start)
+    print_start()
+    # flip() the display to put your work on screen
+    pygame.display.flip()
+
+    # limits FPS to 60
+
+    clock.tick(frames)
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
+# game loop
 while running:
     time += 1
     for event in pygame.event.get():
@@ -30,7 +55,9 @@ while running:
     # put ground on screen
     screen.blit(G.ground1, G.rect1)
     screen.blit(G.ground2, G.rect2)
-    screen.blit(D.d_run1, D.rect_start)
+    # dino move
+    screen.blit(D.d_run, D.rect_run)
+
     D.run(time)
     # move ground
     G.update(velocity)
